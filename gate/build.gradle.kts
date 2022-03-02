6f655a7237829a3cee1183f5b5ca9fe4c9e81d21
Phodal Huang <h@phodal.com>
2022-03-02 08:46:14 +0800

    docs: add comments

--- gate/build.gradle.kts
+++ gate/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
+    // for run kotlin script
     implementation(kotlin("compiler-embeddable"))
     implementation(kotlin("scripting-jsr223"))
     runtimeOnly(kotlin("scripting-compiler-embeddable"))
